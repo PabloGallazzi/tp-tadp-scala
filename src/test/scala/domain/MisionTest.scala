eb337a8874f0ccfc904d57ef41a7fda8893451c9
Mariano <mgnizzero@gmail.com>
2016-06-25 22:35:15 -0300

    Se suben los test corregidos

--- src/test/scala/domain/MisionTest.scala
+++ src/test/scala/domain/MisionTest.scala
@@ -10,8 +10,8 @@ class MisionTest extends BaseTest{
   @Test
   def `test_la_mision_es_realizable`() = {
     val mision: Mision = new Mision(List(tarea, tarea), new masOroParaElEquipo(100))
-    def resultado: Resultado = mision.realizarsePor(equipo)
-    assert(mision.realizarsePor(equipo).isInstanceOf[Exito])
+    def resultado: Resultado = mision.realizarsePor(equipo).get
+    assert(mision.realizarsePor(equipo).get.isInstanceOf[Exito])
     def exito: Exito = resultado.asInstanceOf[Exito]
     assert(exito.equipo != equipo)
 
@@ -20,8 +20,8 @@ class MisionTest extends BaseTest{
   @Test
   def `test_la_mision_no_es_realizable`() = {
     val mision: Mision = new Mision(List(tareaNoRealizable, tarea), new masOroParaElEquipo(100))
-    def resultado: Resultado = mision.realizarsePor(equipo)
-    assert(mision.realizarsePor(equipo).isInstanceOf[Fracaso])
+    def resultado: Resultado = mision.realizarsePor(equipo).get
+    assert(mision.realizarsePor(equipo).get.isInstanceOf[Fracaso])
     def fracaso: Fracaso = resultado.asInstanceOf[Fracaso]
     assert(fracaso.tarea == tareaNoRealizable)
     assert(fracaso.equipo == equipo)
--- src/test/scala/domain/TabernaTest.scala
+++ src/test/scala/domain/TabernaTest.scala
@@ -9,7 +9,7 @@ class TabernaTest extends BaseTest{
 
   @Test
   def `test_la_taberna_no_es_realizable`() = {
-    val mision: Mision = new Mision(List(tarea, tareaNoRealizable), new masOroParaElEquipo(100))
+    val mision: Mision = new Mision(List(tareaNoRealizable), new masOroParaElEquipo(100))
     val taberna: Taberna = new Taberna(List(mision))
     assert(taberna.entrenar(equipo, { (equipo1, equipo2) => equipo1.oro > equipo2.oro }) == equipo)
   }
